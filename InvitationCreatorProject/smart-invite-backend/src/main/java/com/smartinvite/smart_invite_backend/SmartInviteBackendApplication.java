f41d73a0117253996b38202415d95debad993e48
Basanagoud <basanagoudp329@gmail.com>
2025-11-30 17:57:07 +0530

    chore: initialize Spring Boot project and configure MySQL properties

---
+++ InvitationCreatorProject/smart-invite-backend/src/main/java/com/smartinvite/smart_invite_backend/SmartInviteBackendApplication.java
@@ -0,0 +1,14 @@
+package com.smartinvite.smart_invite_backend;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInviteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInviteBackendApplication.class, args);
+	}
+
+}
---
+++ InvitationCreatorProject/smart-invite-backend/src/test/java/com/smartinvite/smart_invite_backend/SmartInviteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartinvite.smart_invite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInviteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
